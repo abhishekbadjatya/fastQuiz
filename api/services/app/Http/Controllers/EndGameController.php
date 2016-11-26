a423ca5b75a25aafaa8a99a40fbd7d51cc9c09c9
parthpanchmatia <psp2137@columbia.edu>
2016-11-25 19:38:06 -0500

    Add files via upload
    Added EndGameController

---
+++ api/services/app/Http/Controllers/EndGameController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Response;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\DB;
+use stdClass;
+use Session;
+use App\Levels;
+use App\Options;
+use App\Questions;
+
+class EndGameController extends Controller {
+	
+	public static function index() {
+
+		$payload = Input::all();
+
+		$payLoadAnswers = $payload['answers'];
+
+		$correctOptionIds = Options::getCorrectOptionIds($payLoadAnswers); 
+		$k = 0;
+		$countOfCorrect = 0;
+		$countOfTotal = 0;
+		$obj1 = new stdClass();
+		$obj1->correctAnswers = array ();
+
+		$questionIdArr = array();
+		foreach ($payLoadAnswers as $answer){
+			array_push($questionIdArr, array('questionId' => $answer['questionId'], 'optionId' => $answer['optionId']));
+		}
+		asort($questionIdArr);
+		
+		foreach ($questionIdArr as $answer){
+			$countOfTotal++;
+			$dbElement = $correctOptionIds[$answer['questionId']];
+			$payloadElement = $answer['optionId'];
+			if($dbElement == $payloadElement){
+				$countOfCorrect++;
+			}
+			
+		    array_push($obj1->correctAnswers, array (
+
+		    		"questionId" => $answer['questionId'],
+		    		"correctAnswerOptionId" => $dbElement 
+		    	));
+		}
+
+		$percent = $countOfCorrect/$countOfTotal;
+
+		if(Session::has('score')){
+			//echo 'Initial: ' . Session::get('score'). '<br/>';
+			$updatedScore = Session::get('score') + $countOfCorrect;
+			//Session::forget('score');
+			Session::put('score', $updatedScore);
+			//echo Session::get('score');
+		}
+		else {
+			//echo 'entered else';
+			Session::put('score', $countOfCorrect);
+		}
+
+		$currentLevel = $payload['level'];
+		$nextLevel = $currentLevel + 1;
+		$maxLevel = Levels::getMaxLevel();
+		$totalScore = Session::get('score');
+
+
+		$obj = new stdClass();
+		$obj->isGameOver = true;
+		$obj->score = $countOfCorrect;
+		$obj->totalScore = $totalScore;
+		$obj->totalNoOfQuestionsInCurrentLevel = $countOfTotal;
+		$obj->previous = array('correctAnswers' => $obj1->correctAnswers);
+	
+		return \Response::json($obj);
+	}
+}
