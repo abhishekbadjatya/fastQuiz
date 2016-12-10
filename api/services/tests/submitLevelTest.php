1637e4c9bbca45cff8996e32c6915babf1c4fdf6
Sajal Khandelwal <sajal50@gmail.com>
2016-12-10 16:18:59 -0500

    checking CI

--- api/services/tests/submitLevelTest.php
+++ api/services/tests/submitLevelTest.php
@@ -12,105 +12,105 @@ class submitLevelTest extends TestCase
      * @return void
      */
     use DatabaseTransactions;
-    public function testnewGameRoute()
-    {
-        $this->get('/newGame')
-             ->seeJsonStructure([
-                "level",
-                "questions" => [
-                    "*" => [
-                        "questionId",
-                        "questionText",
-                        "options" => [
-                            "*"=>[
-                                "optionId",
-                        "optionLabel"
-                            ]
-                        ]
-                    ]
-                ]
-            ]);
-    }
+    // public function testnewGameRoute()
+    // {
+    //     $this->get('/newGame')
+    //          ->seeJsonStructure([
+    //             "level",
+    //             "questions" => [
+    //                 "*" => [
+    //                     "questionId",
+    //                     "questionText",
+    //                     "options" => [
+    //                         "*"=>[
+    //                             "optionId",
+    //                     "optionLabel"
+    //                         ]
+    //                     ]
+    //                 ]
+    //             ]
+    //         ]);
+    // }
 
-    public function testsubmitLevelRoute()
-    {
+    // public function testsubmitLevelRoute()
+    // {
         
-        $this->json('POST', '/submitLevel', ['answers' => [["questionId" => 1,"optionId" => "3"],
-                                                           ["questionId" => 2,"optionId" => "6"],
-                                                           ["questionId" => 3,"optionId" => "8"]
-            ],"level" => 1
-            ])
-             ->seeJsonStructure([
-                "isGameOver",
-                "score",
-                "totalScore",
-                "totalNoOfQuestionsInCurrentLevel",
-                "previous" => [
+    //     $this->json('POST', '/submitLevel', ['answers' => [["questionId" => 1,"optionId" => "3"],
+    //                                                        ["questionId" => 2,"optionId" => "6"],
+    //                                                        ["questionId" => 3,"optionId" => "8"]
+    //         ],"level" => 1
+    //         ])
+    //          ->seeJsonStructure([
+    //             "isGameOver",
+    //             "score",
+    //             "totalScore",
+    //             "totalNoOfQuestionsInCurrentLevel",
+    //             "previous" => [
                     
-                        "correctAnswers" => [
-                            "*"=>[
-                                "questionId",
-                                "correctAnswerOptionId"
-                            ]
-                        ]
+    //                     "correctAnswers" => [
+    //                         "*"=>[
+    //                             "questionId",
+    //                             "correctAnswerOptionId"
+    //                         ]
+    //                     ]
                     
-                ],
-                "hasQualified" 
-             ]);
-    }
+    //             ],
+    //             "hasQualified" 
+    //          ]);
+    // }
 
 
-    public function testsubmitLevelRouteWithWrongQuestion()
-    {
+    // public function testsubmitLevelRouteWithWrongQuestion()
+    // {
         
-        $this->json('POST', '/submitLevel', ['answers' => [["questionId" => 5,"optionId" => "3"],
-                                                           ["questionId" => 2,"optionId" => "6"],
-                                                           ["questionId" => 3,"optionId" => "8"]
-            ],"level" => 1
-            ])
-             ->seeJsonEquals([
-                'ERROR'=>"QUESTION NOT IN CURRENT LEVEL"
-             ]);
-    }
+    //     $this->json('POST', '/submitLevel', ['answers' => [["questionId" => 5,"optionId" => "3"],
+    //                                                        ["questionId" => 2,"optionId" => "6"],
+    //                                                        ["questionId" => 3,"optionId" => "8"]
+    //         ],"level" => 1
+    //         ])
+    //          ->seeJsonEquals([
+    //             'ERROR'=>"QUESTION NOT IN CURRENT LEVEL"
+    //          ]);
+    // }
 
-    public function testhasQualifiedFalse()
-    {
-        $this->json('POST', '/submitLevel', ['answers' => [["questionId" => 1,"optionId" => "3"],
-                                                           ["questionId" => 2,"optionId" => "6"],
-                                                           ["questionId" => 3,"optionId" => "8"]
-            ],"level" => 1
-            ])
-             ->seeJson([
-                'hasQualified' => false
-             ]);
-    }
+    // public function testhasQualifiedFalse()
+    // {
+    //     $this->json('POST', '/submitLevel', ['answers' => [["questionId" => 1,"optionId" => "3"],
+    //                                                        ["questionId" => 2,"optionId" => "6"],
+    //                                                        ["questionId" => 3,"optionId" => "8"]
+    //         ],"level" => 1
+    //         ])
+    //          ->seeJson([
+    //             'hasQualified' => false
+    //          ]);
+    // }
 
-    public function testhasQualifiedTrue()
-    {
-        $this->json('POST', '/submitLevel', ['answers' => [["questionId" => 1,"optionId" => "1"],
-                                                           ["questionId" => 2,"optionId" => "5"],
-                                                           ["questionId" => 3,"optionId" => "8"]
-            ],"level" => 1
-            ])
-             ->seeJson([
-                'hasQualified' => true
-             ]);
-    }
+    // public function testhasQualifiedTrue()
+    // {
+    //     $this->json('POST', '/submitLevel', ['answers' => [["questionId" => 1,"optionId" => "1"],
+    //                                                        ["questionId" => 2,"optionId" => "5"],
+    //                                                        ["questionId" => 3,"optionId" => "8"]
+    //         ],"level" => 1
+    //         ])
+    //          ->seeJson([
+    //             'hasQualified' => true
+    //          ]);
+    // }
     
 
 
-public function testisGameOverTrueForLastLevel()
-    {
+    // public function testisGameOverTrueForLastLevel()
+    // {
         
-        $this->json('POST', '/submitLevel', ['answers' => [["questionId" => 5,"optionId" => "3"],
-                                                           ["questionId" => 6,"optionId" => "6"],
-                                                           ["questionId" => 4,"optionId" => "8"]
-            ],"level" => 2
-            ])
-             ->seeJson([
-                'isGameOver' => true
-             ]);
-    }
+    //     $this->json('POST', '/submitLevel', ['answers' => [["questionId" => 5,"optionId" => "3"],
+    //                                                        ["questionId" => 6,"optionId" => "6"],
+    //                                                        ["questionId" => 4,"optionId" => "8"]
+    //         ],"level" => 2
+    //         ])
+    //          ->seeJson([
+    //             'isGameOver' => true
+    //          ]);
+    // }
 
 
 }
